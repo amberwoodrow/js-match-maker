4368a1d5bf2c44272b5982339f9a048ed499353a
amberwoodrow <amberwoodrow@gmail.com>
2015-07-19 18:57:39 -0600

    made a reusable function to get student and mentor info both.

--- main.js
+++ main.js
@@ -1,87 +1,49 @@
+function getInfo(infoType) {
+  do {
+    var numPeople = prompt("What is the total number of "+ infoType +"s learning JavaScript?");
+  } while (numPeople < 0 || !numPeople.match(/\d/g));
 
-do {
-  var numStudents = prompt("What is the total number of Students learning JavaScript?");
-} while (numStudents < 0 || !numStudents.match(/\d/g));
+  var i = 0;
+  var people = [];
 
-var i = 0;
-var students = [];
+  while (i < numPeople) {
+    var personInfo = {};
+    i++;
+    var error = false;
 
-while (i < numStudents) {
-  var studentInfo = {};
-  i++;
-  var error = false;
+    do {
+      if (error) {
+        alert("Please enter a valid name");
+      }
+      personInfo.name = prompt("Please enter the " + infoType + "'s name.");
+      error = true;
+    } while (!personInfo.name.match(/[a-zA-Z ]/) || personInfo.name === null);
 
-  do {
-    if (error) {
-      alert("Please enter a valid name");
-    }
-    studentInfo.name = prompt("Please enter the student's name.");
-    error = true;
-  } while (!studentInfo.name.match(/[a-zA-Z ]/) || studentInfo.name === null);
+    error = false;
+    do {
+      if (error) {
+        alert("Please enter a valid phone number.");
+      }
+      personInfo.phone = prompt("Please enter the " + infoType + "'s phone number in this format 123-456-7890.");
+      error = true;
+    } while (!personInfo.phone.match(/^\d{3}-\d{3}-\d{4}$/));
 
-  error = false;
-  do {
-    if (error) {
-      alert("Please enter a valid phone number.");
-    }
-    studentInfo.phone = prompt("Please enter the student's phone number in this format 123-456-7890.");
-    error = true;
-  } while (!studentInfo.phone.match(/^\d{3}-\d{3}-\d{4}$/));
+    error = false;
+    do {
+      if (error) {
+        alert("Please enter a valid city.");
+      }
+      personInfo.city = prompt("Please enter the " + infoType + "'s city.");
+      error = true;
+    } while (!personInfo.city.match(/[a-zA-Z ]/) || personInfo.city === null);
 
-  error = false;
-  do {
-    if (error) {
-      alert("Please enter a valid city.");
-    }
-    studentInfo.city = prompt("Please enter the student's city.");
-    error = true;
-  } while (!studentInfo.city.match(/[a-zA-Z ]/) || studentInfo.city === null);
+    people.push(personInfo);
+  }
 
-  students.push(studentInfo);
+  return "Number of " + infoType + "s: " + numPeople + "<br/>"+ infoType +": " + JSON.stringify(people);
 }
 
-document.write("Number of students: " + numStudents + "<br/>Students: " + JSON.stringify(students));
-
-
-do {
-  var numMentors = prompt("What is the total number of Mentors?");
-} while (numMentors < 0 || !numMentors.match(/\d/g));
-
-var i = 0;
-var mentors = [];
-
-while (i < numMentors) {
-  var mentorInfo = {};
-  i++;
-  var error = false;
-
-  do {
-    if (error) {
-      alert("Please enter a valid name");
-    }
-    mentorInfo.name = prompt("Please enter the mentors's name.");
-    error = true;
-  } while (!mentorInfo.name.match(/[a-zA-Z ]/) || mentorInfo.name === null);
-
-  error = false;
-  do {
-    if (error) {
-      alert("Please enter a valid phone number.");
-    }
-    mentorInfo.phone = prompt("Please enter the mentor's phone number in this format 123-456-7890.");
-    error = true;
-  } while (!mentorInfo.phone.match(/^\d{3}-\d{3}-\d{4}$/));
-
-  error = false;
-  do {
-    if (error) {
-      alert("Please enter a valid city.");
-    }
-    mentorInfo.city = prompt("Please enter the student's city.");
-    error = true;
-  } while (!mentorInfo.city.match(/[a-zA-Z ]/) || mentorInfo.city === null);
-
-  mentors.push(mentorInfo);
-}
-
-document.write("<br/>Number of mentors: " + numMentors + "<br/>Mentors: " + JSON.stringify(mentors));
+students = getInfo("Student");
+mentor = getInfo("Mentor");
+document.write(students);
+document.write(mentor);
